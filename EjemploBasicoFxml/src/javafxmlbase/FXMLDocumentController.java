7217bf17d02ef3ea99906a545b5ba37447f9ab01
Ignacio-Rueda <nachord@outlook.com>
2024-04-07 22:55:39 +0200

    Ejemplo básico.

--- EjemploBasicoFxml/src/javafxmlbase/FXMLDocumentController.java
+++ EjemploBasicoFxml/src/javafxmlbase/FXMLDocumentController.java
@@ -5,31 +5,40 @@
  */
 package javafxmlbase;
 
+import com.sun.javafx.scene.shape.TextHelper;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author jjber
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField textNombre;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextArea txtArea;
+
+    @FXML
+    private void mostrar(ActionEvent event) {
+        txtArea.setText(textNombre.getText());
     }
+    @FXML
+    private void borrar(ActionEvent event){
+        txtArea.setText("");
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
